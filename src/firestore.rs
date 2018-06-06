2a7874a87c990ca63ba5e500c793f7896ae6fead
600913 <600913@gmail.com>
2018-06-06 18:02:35 +0900

    add functional

--- src/firestore.rs
+++ src/firestore.rs
@@ -5,7 +5,7 @@ use yew::callback::Callback;
 impl FirebaseService {
     // Cloud Firestore
     #[allow(non_snake_case)]
-    pub fn collectionGet(&mut self, collection: &str, doc: &str, callback: Callback<Value>) {
+    pub fn collection_doc(&mut self, collection: &str, doc: &str, callback: Callback<Value>) {
         let callback = move |val| {
             callback.emit(val);
         };
@@ -25,6 +25,29 @@ impl FirebaseService {
             }).catch(function(error) {
                 console.log("Error getting document:", error);
                 callback.drop();
+            });
+        };
+    }
+
+    // Cloud Firestore
+    #[allow(non_snake_case)]
+    pub fn collection(&mut self, collection: &str, callback: Callback<Value>) {
+        let callback = move |val| {
+            callback.emit(val);
+        };
+        js! {
+            var callback = @{ callback };
+            var db = firebase.firestore();
+            var docRef = db.collection(@{collection});
+            docRef.get().then(function(querySnapshot) {
+                var videos = [];
+                querySnapshot.forEach(function(doc) {
+                    // doc.data() is never undefined for query doc snapshots
+                    // console.log(doc.id, " => ", doc.data());
+                    videos.push[doc.data()["url"]];
+                });
+                callback(videos);
+                // callback.drop()
             });
         };
     }
